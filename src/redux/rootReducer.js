c3db747b260df7546aa7646ca1fcffa62209188d
JatinMehra <mehrajatin142002@gmail.com>
2022-07-11 02:26:35 +0530

    Async actions with React redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cakeReducer from "./cakes/cakeReducers";
 import iceCreamReducer from "./iceCream/iceCreamReducers";
+import reducer from "./user/userReducers";
 
 const rootReducer = combineReducers({
     cake: cakeReducer,
-    iceCream : iceCreamReducer
+    iceCream : iceCreamReducer,
+    user: reducer
 })
 
 export default rootReducer;
