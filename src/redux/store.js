cc1251fd32e3a8371d24e5fe2b47126bab69cf1c
JatinMehra <mehrajatin142002@gmail.com>
2022-07-10 17:03:01 +0530

    Redux-devtool added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import {legacy_createStore , applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
-const store = legacy_createStore(rootReducer, applyMiddleware(logger));
+// const store = legacy_createStore(rootReducer, applyMiddleware(logger));
+
+const store = legacy_createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
