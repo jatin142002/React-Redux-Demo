cacb29dfc252bf71fc4716b28dccc6313ae36747
JatinMehra <mehrajatin142002@gmail.com>
2022-07-10 12:47:13 +0530

    redux with react hook v2 - useDispatch()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CakeContainer from './components/CakeContainer.js';
+import HooksCakeContainer from './components/HooksCakeContainer.js';
 import {Provider} from 'react-redux';
 import store from './redux/store.js';
 import './App.css';
@@ -7,6 +8,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { buyCake } from '../redux/index.js';
 
 function HooksCakeContainer() {
 
     // this hook accepts function as an argument and i.e called as selector function this function accepts
     // redux state as its argument 
-    const numofCakes = useSelector(state => state.numOfCakes)
+    const numofCakes = useSelector(state=>state.numofCakes);
+    const dispatch = useDispatch();
 
     return (
         <div>
             <h2>Number of cakes - {numofCakes}</h2>
-            <button>Buy Cake</button>
+            <button onClick={()=> dispatch(buyCake())}>Buy Cake</button>
         </div>
     )
 }
